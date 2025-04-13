c45ed556b18e5671d478f576805203bc732b7bc1
chenq0000 <chenqiang@chenqiangs-Mac-mini.local>
2025-04-14 05:55:34 +0800

    can build

--- webapp/packages/chat-sdk/src/index.tsx
+++ webapp/packages/chat-sdk/src/index.tsx
@@ -1,14 +1,14 @@
 import './styles/index.less';
 
-import ReactDOM from 'react-dom/client';
-import Chat from './demo/Chat';
-import ChatDemo from './demo/ChatDemo';
-import CopilotDemo from './demo/CopilotDemo';
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<div>
-  <ChatDemo />
-  <CopilotDemo />
-</div>);
+// import ReactDOM from 'react-dom/client';
+// import Chat from './demo/Chat';
+// import ChatDemo from './demo/ChatDemo';
+// import CopilotDemo from './demo/CopilotDemo';
+// const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+// root.render(<div>
+//   <ChatDemo />
+//   <CopilotDemo />
+// </div>);
 
 export { default as Chat } from './Chat';
 
